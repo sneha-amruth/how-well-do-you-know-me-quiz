f39b6a2b9771191b29ee6425648737bb3b47d2e2
Sneha Amruth <sneha.amruth24@gmail.com>
2020-11-27 23:20:54 +0530

    feat: added highscore feat

---
+++ index.js
@@ -0,0 +1,84 @@
+var readlinesync = require('readline-sync');
+const chalk = require('chalk');
+
+var userName = readlinesync.question(chalk.white.bgBlue.bold("Welcome, Please enter your name "));
+
+console.log(chalk.white.bgBlue.bold("\n"+userName+", let's see how well do you know Sneha\n"));
+
+
+var listOfQuestions = [{
+  question: "Where do i live?",
+  correctAnswer: 'hyderabad'
+},
+{
+  question: "Do I like mountains or beaches?",
+  correctAnswer: 'mountains'
+},
+{
+  question: "My fav food item?",
+  correctAnswer: 'pasta'
+},
+{
+  question: "Am I an early riser or a night owl?",
+  correctAnswer: 'night owl'
+},
+{
+  question: "Do I like Chai or Coffee?",
+  correctAnswer: 'chai'
+},
+]
+var currentScore = 0;
+
+var highscores = [
+  {
+    name: "Sukruthi",
+    score: 1
+  },
+  {
+    name: "Deepa",
+    score: 6
+  },
+  {
+    name: "Kyathi",
+    score: 3
+  },
+]
+
+
+
+listOfQuestions.forEach(function(eachQues){
+  var usersAns = readlinesync.question(eachQues.question+" ");
+  if(usersAns.toLowerCase() == eachQues.correctAnswer){
+     currentScore++;
+     console.log("\nYou're right!\nCurrent Score: "+currentScore);
+  }
+  else{
+    currentScore--;
+    console.log("\nYou're wrong!\nCurrent Score: "+currentScore);
+  }
+  console.log("-------------------------------");
+})
+
+var currHighScore = 0;
+highscores.forEach(function(highscore){
+  if(currentScore >= highscore.score) {
+     currHighScore = currentScore;
+  }
+})
+if(currHighScore != 0){
+  console.log("\nWofoo!\n");
+   highscores.push({name: userName.toString(), score: currentScore.toString()});
+}
+sortScores();
+function sortScores() {
+  highscores.sort(function(a, b){return b.score - a.score});
+}
+  console.log("Leader Board:\n");
+  highscores.forEach(function(highscore){
+    console.log(highscore.name+" with score "+highscore.score+"\n");
+  })
+
+   
+
+
+
